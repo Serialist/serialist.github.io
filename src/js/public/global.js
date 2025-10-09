24581db5bf3434ad00b87197b7168e5869af0301
Serialist <ba3pt@chd.edu.cn>
2025-10-10 00:48:38 +0800

    change to MkDocs

--- src/js/public/global.js
+++
@@ -1,7 +0,0 @@
-
-{
-    var navigation_drawer = document.querySelector("mdui-navigation-drawer");
-    document.querySelector("#menu_button").addEventListener("click", () => {
-        navigation_drawer.open = !navigation_drawer.open;
-    });
-}
